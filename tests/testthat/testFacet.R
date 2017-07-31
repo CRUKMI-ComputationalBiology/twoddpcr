8a1457cb08ccf9835fc9ed27911dac3dbdc4dbb0
Anthony Chiu <anthony.chiu@cruk.manchester.ac.uk>
2017-07-31 11:42:21 +0000

    facetPlot tests
    From: Anthony Chiu <anthony.chiu@cruk.manchester.ac.uk>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/twoddpcr@131665 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/testthat/testFacet.R
@@ -0,0 +1,16 @@
+context("Facet plot")
+
+krasPlate <- ddpcrPlate(KRASdata)
+
+test_that("Facet plot gives a output with correct parameters", {
+  expect_s3_class(facetPlot(krasPlate), c("gg", "ggplot"))
+  expect_s3_class(facetPlot(krasPlate, cMethod=NULL), c("gg", "ggplot"))
+  expect_s3_class(facetPlot(krasPlate, cMethod="Cluster"), c("gg", "ggplot"))
+  expect_equal(facetPlot(krasPlate), facetPlot(krasPlate, cMethod=NULL))
+})
+
+test_that("Facet plot fails when plotting the wrong column", {
+  expect_error(facetPlot(krasPlate, cMethod="Well"))
+  expect_error(facetPlot(krasPlate, cMethod="AAA"))
+})
+
